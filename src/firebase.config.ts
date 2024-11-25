3570bff63a6e2e41bb32836efeb19cd7f42f9c03
vinileann <vinileann@gmail.com>
2024-11-25 09:36:06 -0300

    integração com firestore feita, so por codigo que há a adiçao de emails por enquanto

---
+++ src/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAxkg66mbEBTP4wIrsfl27NRZMfOvRkwK8",
+    authDomain: "lpsmartbid.firebaseapp.com",
+    projectId: "lpsmartbid",
+    storageBucket: "lpsmartbid.firebasestorage.app",
+    messagingSenderId: "52943907855",
+    appId: "1:52943907855:web:1fed377c336e70de7f077a",
+    measurementId: "G-M389YE47M2"
+};
